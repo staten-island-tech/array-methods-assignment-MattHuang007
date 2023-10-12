e3575e7642adad59c7e9458ae5da6373ad31d1bc
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-10-12 13:11:12 -0400

    n

---
+++ index.js
@@ -0,0 +1,24 @@
+const animals =[
+    {
+    name: "crab",
+    scientificname:"Brachyura",
+    environment: "ocean",
+    skeleton:false,
+    exoskeleton:true,
+    },
+    {
+    name:"clam",
+    scientificname:"Bivalvia",
+    environment:"ocean",
+    skeleton:false,
+    exoskeleton:true, 
+    },
+    {
+    name:"dog",
+    scientificname:"Canis Lupus Familiaris",
+    environment:"domestic",
+    skeleton:true,
+    exoskeleton:false,
+    },
+    
+]
