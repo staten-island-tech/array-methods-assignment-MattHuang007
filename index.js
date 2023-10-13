7df9027a7c03e0069ccb7c8f81abe771ce9ae3f8
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-10-13 13:15:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,25 @@ const animals =[
     skeleton:true,
     exoskeleton:false,
     },
-    
+    {
+    name:"cat",
+    scientificname:"Felis Catus",
+    environment:"domestic",
+    skeleton:true,
+    exoskeleton:false,
+    },
+    {
+    name:"frog",
+    scientificname:"Anura",
+    environment:"freshwater",
+    skeleton:true,
+    exoskeleton: false,
+    },
+    {
+    name:"chimpanzee",
+    scientificname:"Pan Troglodytes",
+    environment:"jungle",
+    skeleton:true,
+    exoskeleton: false,
+    },
 ]
